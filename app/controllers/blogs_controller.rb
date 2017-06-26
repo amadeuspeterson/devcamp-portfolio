7c1a2fff13578f83c0d8d47646a418b2de3ba90f
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:32:48 +0000

    more bug fixes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,6 +29,10 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+      
+      else
+        format.html { render :new }
+        
       end
     end
   end
@@ -39,6 +43,10 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        
+      else
+        format.html { render :edit }
+        
       end
     end
   end
@@ -48,7 +56,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed' }
       format.json { head :no_content }
     end
   end
@@ -58,8 +66,8 @@ class BlogsController < ApplicationController
       @blog.published!
       elsif @blog.published?
       @blog.draft!
-  end
-  
+    end
+      
     redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
@@ -73,4 +81,4 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
-  end
+end
