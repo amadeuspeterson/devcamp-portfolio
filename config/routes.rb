522bb38c59dd1ab90b44864aa8ca0603a2ed296a
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 17:06:24 +0000

    seeds and routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
-
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
